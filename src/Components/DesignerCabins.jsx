732b3569d7ddd66d5999d37fdebf5a1c4ecfc867
WebWorldHub0009 <projectswebworldhub009@gmail.com>
2025-11-22 12:40:00 +0530

    another commit

--- src/Components/DesignerCabins.jsx
+++ src/Components/DesignerCabins.jsx
@@ -1,4 +1,3 @@
-// src/pages/DesignerCabins.jsx
 import React, { useState, useEffect, useCallback } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { Link, useNavigate } from "react-router-dom";
